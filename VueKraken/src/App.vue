9a1dfed02b771359a4e7c78f024afa1a0a5e1ea0
Rob <robert.systemdeveloper@gmail.com>
2021-03-13 14:20:54 +0100

    Added code to App.vue for route and store handling

--- VueKraken/src/App.vue
+++ VueKraken/src/App.vue
@@ -1,11 +1,25 @@
 <template>
+  <main>
+    <Navbar />
+    <router-view />
+  </main>
 </template>
 
 <script>
+import Navbar from "./components/Navbar.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-  }
-}
+    Navbar,
+  },
+
+  // There is in total 8 different hooks in vue. Each hook represent a lifecycle on vue
+  // component. Here we use "created" because we want to grab the data from 
+  // our server as soon as possible. 
+  created() {
+    this.$store.dispatch("initCars");
+  },
+};
 </script>
+
